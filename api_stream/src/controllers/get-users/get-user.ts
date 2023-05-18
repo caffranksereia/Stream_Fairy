d5ea563c4014a2798a39acbed4bc211f0224415c
caffranksereia <fabio@yaayoo.com.br>
2023-05-18 14:33:28 -0300

    vai pegar uma lista de usuario e aparecer

--- api_stream/src/controllers/get-users/get-user.ts
+++ api_stream/src/controllers/get-users/get-user.ts
@@ -4,7 +4,15 @@ export class GetUsersController implements IGetUsersContollers {
     constructor(private readonly getUsersRepository: IGetUsersResponsitory) {
         this.getUsersRepository = getUsersRepository
     }
-    handle() {
-
+    async handle() {
+        try {const users = await this.getUsersRepository.getUsers()
+                    return {statusCode: 200,
+                                                body: users 
+                                            }
+                                        } catch(error){
+                                            return {statusCode: 500,
+                                                body: "Something went wrong" 
+                                            }
+        
     }
 }
