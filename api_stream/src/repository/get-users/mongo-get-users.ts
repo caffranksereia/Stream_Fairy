d394f41f6ebd0481f7a765f40ee3954c16a61da0
caffranksereia <fabio@yaayoo.com.br>
2023-05-18 15:23:46 -0300

    chamando o banco para a collectio de uma array

--- api_stream/src/repository/get-users/mongo-get-users.ts
+++ api_stream/src/repository/get-users/mongo-get-users.ts
@@ -1,9 +1,13 @@
 import { ADDRGETNETWORKPARAMS } from "dns";
 import { IGetUsersResponsitory } from "../../controllers/get-users/protocols";
 import { User } from "../../models/user";
+import { MongoClient } from "../../database/mongo";
 
 export class MongoGetUsersRepository implements IGetUsersResponsitory {
     async getUsers(): Promise<User[]> {
+        const users = await MongoClient.db.collection<User>('users').find({}).toArray();
+
+        
         return [
             {
                 name: "admin",
