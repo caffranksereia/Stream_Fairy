19f197dc9e8ece9dcba625957c80470bf38499aa
caffranksereia <fabio@yaayoo.com.br>
2023-05-18 14:09:45 -0300

    rotas do swagger e primeiras configs

--- api_stream/src/index.ts
+++ api_stream/src/index.ts
@@ -1,14 +1,14 @@
 import express, { Response, Request, Router}  from "express"
 import * as dotenv from "dotenv"
-
+import swaggerUi from "swagger-ui-express"
 dotenv.config();
-
+import swaggerDocs from "./swagger.json"
 const router = Router();
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
+app.use("swaggerUi",swaggerUi.serve, swaggerUi.setup(swaggerDocs) )
 app.get('/', (req, res) => {
     res.send(":hello world")
 })
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, ()=>{
-    console.log(`console.log${PORT}`)
+    console.log(`${PORT}`)
 })
