6fdc5d2cf473d696f9a77f3ec39cef51d1dc2395
caffranksereia <fabio@yaayoo.com.br>
2023-05-18 14:19:08 -0300

    swagger removido

--- api_stream/src/index.ts
+++ api_stream/src/index.ts
@@ -2,13 +2,13 @@ import express, { Response, Request, Router}  from "express"
 import * as dotenv from "dotenv"
 import swaggerUi from "swagger-ui-express"
 dotenv.config();
-import swaggerDocs from "./swagger.json"
+// import swaggerDocs from "./swagger.json"
 const router = Router();
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use("swaggerUi",swaggerUi.serve, swaggerUi.setup(swaggerDocs) )
+// app.use("swaggerUi",swaggerUi.serve, swaggerUi.setup(swaggerDocs) )
 app.get('/', (req, res) => {
     res.send(":hello world")
 })
