9e808fddb7f3b92d84987dfc77c54ba88c69a2e3
caffranksereia <fabio@yaayoo.com.br>
2023-05-18 13:52:04 -0300

    dotenv config, router, app express e use e a porta

--- api_stream/src/index.ts
+++ api_stream/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Response, Request, Router}  from "express"
+import * as dotenv from "dotenv"
 
+dotenv.config();
 
 const router = Router();
 
 const app = express()
 app.use(express.json())
-
+app.use(express.urlencoded({extended:false}))
 
 
 
