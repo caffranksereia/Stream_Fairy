4ce488221e2630095d817fc565e1754fbc12dee8
caffranksereia <fabio@yaayoo.com.br>
2023-05-18 13:55:32 -0300

    app get hello world

--- api_stream/src/index.ts
+++ api_stream/src/index.ts
@@ -9,7 +9,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-
+app.get('/', (req, res) => {
+    res.send(":hello world")
+})
 
 const PORT = process.env.PORT 
 
