6502b6d0e94d76aee92993ab647c9814b80723cd
caffranksereia <fabio@yaayoo.com.br>
2023-05-19 09:21:48 -0300

    feat: criacao da rota e criacao da rota movie

--- api_stream/src/index.ts
+++ api_stream/src/index.ts
@@ -1,11 +1,9 @@
 import express, { Response, Request, Router, response}  from "express"
 import * as dotenv from "dotenv"
 import swaggerUi from "swagger-ui-express"
-import { MongoGetUsersRepository } from "./repository/get-users/mongo-get-users";
-import { GetUsersController } from "./controllers/get-users/get-user";
-import { MongoClient } from "./database/mongo";
-import { MongoCreateUserRepository } from "./repository/post-users/mongo-create-user";
-import { CreateUserController } from "./controllers/create-user/create-user";
+import { MongoCreateUserRepository, MongoGetUsersRepository, MongoUpdateUserRepository, MongoDeleteUserRepository, MongoGetMovieRepository, MongoCreateMovieRepository, MongoUpdateMovieRepository, MongoDeleteMovieRepository} from "./repository"
+import { CreateUserController, GetUsersController, UpdateUserController, DeleteUserController, GetMovieController, CreateMovieController, UpdateMovieController, DeleteMovieController} from "./controllers"
+import { MongoClient } from "./database"
 
 
 const main = async () =>{
@@ -21,7 +19,7 @@ const main = async () =>{
 
 
     await MongoClient.connect()
-
+ // Users route
     app.get('/', (req, res) => {
         res.send(":hello world")
     })
@@ -45,7 +43,78 @@ const main = async () =>{
     
         res.send(body).status(statusCode)
     })
+    app.patch('/users/:id', async (req, res) => {
+        const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+    
+        const updateUserController= new UpdateUserController(mongoUpdateUserRepository) 
+    
+        const {body, statusCode} = await updateUserController.handle({body: req.body, params: req.params})
+    
+        res.send(body).status(statusCode)
+    })
 
+    app.delete("/users/:id", async (req, res) => {
+        const mongoDeleteUserRepository = new MongoDeleteUserRepository();
+    
+        const deleteUserController = new DeleteUserController(
+          mongoDeleteUserRepository
+        );
+    
+        const { body, statusCode } = await deleteUserController.handle({
+          params: req.params,
+        });
+    
+        res.status(statusCode).send(body);
+      });
+
+      // Movie route
+
+
+    app.get('/movie', async (req, res) => {
+        const mongoGetMovieRepository = new MongoGetMovieRepository();
+    
+        const getMovieController= new GetMovieController(mongoGetMovieRepository) 
+    
+        const {body, statusCode} = await getMovieController.handle()
+    
+        res.send(body).status(statusCode)
+    })
+
+    app.post('/movie', async (req, res) => {
+        const mongoMovieRepository = new MongoCreateMovieRepository();
+    
+        const createMovieController= new CreateMovieController(mongoMovieRepository) 
+    
+        const {body, statusCode} = await createMovieController.handle({body: req.body})
+    
+        res.send(body).status(statusCode)
+    })
+
+    app.patch('/movie/:id', async (req, res) => {
+        const mongoUpdateMovieRepository = new MongoUpdateMovieRepository();
+    
+        const updateMovieController= new UpdateMovieController(mongoUpdateMovieRepository) 
+    
+        const {body, statusCode} = await updateMovieController.handle({body: req.body, params: req.params})
+    
+        res.send(body).status(statusCode)
+    })
+
+    app.delete("/users/:id", async (req, res) => {
+        const mongoDeleteMovieRepository = new MongoDeleteMovieRepository();
+    
+        const deleteUserController = new DeleteMovieController(
+            mongoDeleteMovieRepository
+        );
+    
+        const { body, statusCode } = await deleteUserController.handle({
+          params: req.params,
+        });
+    
+        res.status(statusCode).send(body);
+      });
+
+    
     const PORT = process.env.PORT || 4000
 
 app.listen(PORT, ()=>{
