a213bddf5281d607ca1c19b9825a736c1ee0f159
caffranksereia <fabio@yaayoo.com.br>
2023-05-19 09:18:22 -0300

    feat: delete

--- api_stream/src/controllers/get-users/protocols.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../../models/user"
-import {HttpResponde} from "../protocols"
-export interface IGetUsersContollers {
-    handle():Promise<HttpResponse<User[]>>
-}
-
-export interface IGetUsersResponsitory {
-    getUsers(): Promise<User[]>
-}
