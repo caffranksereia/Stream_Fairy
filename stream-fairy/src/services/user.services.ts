98c09049b93ee5c21792b61d80f63998ccbfce1c
caffranksereia <fabio@yaayoo.com.br>
2023-05-19 14:11:17 -0300

    feat: class User service

---
+++ stream-fairy/src/services/user.services.ts
@@ -0,0 +1,17 @@
+import { api } from "api-config.service";
+import authHeader from "./auth-header";
+
+
+class UserService {
+    getPublicContent() {
+        return api.get('all')
+    }
+    getUserBoard() {
+        return api.get('user', {headers: authHeader()})
+    }
+    getAdmBoard() {
+        return api.get('admin', {headers: authHeader()})
+    }
+}
+
+export default new UserService()]
