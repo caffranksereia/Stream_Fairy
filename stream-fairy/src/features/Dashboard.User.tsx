29888f76b98e139a5399f31e7dd0cf5cc02c3123
caffranksereia <fabio@yaayoo.com.br>
2023-05-19 13:08:57 -0300

    feat: dashboard user

---
+++ stream-fairy/src/features/Dashboard.User.tsx
@@ -0,0 +1,12 @@
+
+import { LoginComponent } from "components/Login-Component";
+import React from "react";
+
+
+export default function DashboardUser(){
+return (
+    <div>
+        <LoginComponent/>
+    </div>
+)
+    }
