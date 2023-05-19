9b326c2631f3e58f1eed1c351896af69317dc9ad
caffranksereia <fabio@yaayoo.com.br>
2023-05-19 15:23:22 -0300

    feat: implementei o dashboard adm

--- stream-fairy/src/components/Dashboard-Adm-Component.tsx
+++ stream-fairy/src/components/Dashboard-Adm-Component.tsx
@@ -1,20 +1,48 @@
-import React,{ FC } from "react"
-import { ListComponentMovie } from "./List-Component.Movie"
+import { Component } from "react";
+import userServices from "services/user.services";
 
-export const  DashboardAdmComponent: FC= ()=>{
+type Props = {};
 
+type State = {
+  content: string;
+}
 
+export default class DashboardAdmComponents extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  componentDidMount() {
+    userServices.getAdmBoard().then(
+      response => {
+        this.setState({
+          content: response.data
+        });
+      },
+      error => {
+        this.setState({
+          content:
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString()
+        });
+      }
+    );
+  }
+
+  render() {
     return (
-        <div>
-            <div>
-                <h1>Welcome Administrator </h1>
-            </div>
-            <div>
-                <h1>Menu</h1>
-            </div>
-            <div>
-              <ListComponentMovie/>
-            </div>
-        </div>
-    )
-    }
+      <div className="container">
+        <header className="jumbotron">
+          <h3>{this.state.content}</h3>
+        </header>
+      </div>
+    );
+  }
+}
