5206da672cf39f76b5d888e7b7ae55a32b145482
caffranksereia <fabio@yaayoo.com.br>
2023-05-19 13:09:08 -0300

    feat: dashboard admin

---
+++ stream-fairy/src/features/Dashboard.Adm.tsx
@@ -0,0 +1,12 @@
+
+import { LoginComponent } from "components/Login-Component";
+import React from "react";
+
+
+export default function DashboardAdministrator(){
+return (
+    <div>
+        <LoginComponent/>
+    </div>
+)
+    }
