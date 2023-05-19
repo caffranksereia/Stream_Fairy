1f89e72486579f79c19d61359ef36d89f46bbdd1
caffranksereia <fabio@yaayoo.com.br>
2023-05-19 17:25:40 -0300

    feat: mudei a variavel do profile

--- stream-fairy/src/components/Profile-Component.tsx
+++ stream-fairy/src/components/Profile-Component.tsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { Navigate } from "react-router-dom";
 import AuthService from "../services/auth.service";
 import { IUser } from "interfaces/IUser";
+import authService from "../services/auth.service";
 
 type Props = {};
 
@@ -10,60 +11,30 @@ type State = {
   userReady: boolean,
   currentUser: IUser & { accessToken: string }
 }
-export default class ProfileComponents extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      redirect: null,
-      userReady: false,
-      currentUser: { accessToken: "" }
-    };
-  }
-
-  componentDidMount() {
-    const currentUser = AuthService.getCurrentUser();
-
-    if (!currentUser) this.setState({ redirect: "/home" });
-    this.setState({ currentUser: currentUser, userReady: true })
-  }
-
-  render() {
-    if (this.state.redirect) {
-      return <Redirect to={this.state.redirect} />
-    }
-
-    const { currentUser } = this.state;
-
+export const  ProfileComponents: React.FC = () => {
+const currentUser = authService.getCurrentUser();
     return (
-      <div className="container">
-        {(this.state.userReady) ?
-          <div>
-            <header className="jumbotron">
-              <h3>
-                <strong>{currentUser.username}</strong> Profile
-              </h3>
-            </header>
-            <p>
-              <strong>Token:</strong>{" "}
-              {currentUser.accessToken.substring(0, 20)} ...{" "}
-              {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-            </p>
-            <p>
-              <strong>Id:</strong>{" "}
-              {currentUser.id}
-            </p>
-            <p>
-              <strong>Email:</strong>{" "}
-              {currentUser.email}
-            </p>
-            <strong>Authorities:</strong>
-            <ul>
-              {currentUser.roles &&
-                currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-            </ul>
-          </div> : null}
+        <div className="container">
+        <header className="jumbotron">
+          <h3>
+            <strong>{currentUser.username}</strong> Profile
+          </h3>
+        </header>
+        <p>
+          <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
+          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+        </p>
+        <p>
+          <strong>Id:</strong> {currentUser.id}
+        </p>
+        <p>
+          <strong>Email:</strong> {currentUser.email}
+        </p>
+        <strong>Authorities:</strong>
+        <ul>
+          {currentUser.roles &&
+            currentUser.roles.map((role: string, index: number) => <li key={index}>{role}</li>)}
+        </ul>
       </div>
     );
   }
-}
