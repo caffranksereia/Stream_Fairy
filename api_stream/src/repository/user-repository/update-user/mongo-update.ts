97cb1b4b7c6b2c596b93098e57beb92582dcca39
caffranksereia <fabio@yaayoo.com.br>
2023-05-20 10:27:47 -0300

    feat: id mongo update user

--- api_stream/src/repository/user-repository/update-user/mongo-update.ts
+++ api_stream/src/repository/user-repository/update-user/mongo-update.ts
@@ -1,12 +1,12 @@
 import { ObjectId } from "mongodb"
-import { IUpdateUserRepository, UpdateUserParams } from "../../../controllers/user-controller/update-user/protocols";
+import { IUpdateUserRepository, IUpdateUserParams } from "../../../controllers/user-controller/update-user/protocols";
 import { MongoClient } from "../../../database/mongo";
 import { User } from "../../../models/user";
 import { MongoUser } from "../../mongo-protocols";
 
 export class MongoUpdateUserRepository implements IUpdateUserRepository {
-    async updateUser(id: string , params: UpdateUserParams): Promise<User> {
-        await MongoClient.db.collection("user").updateOne(
+    async updateUser(id: string , params: IUpdateUserParams): Promise<User> {
+        await MongoClient.db.collection("users").updateOne(
             {_id: new ObjectId(id)},
             {
                 $set: {
