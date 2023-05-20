5b8ccb6ce8087ff99209c1d2a4f89b4c1156b562
caffranksereia <fabio@yaayoo.com.br>
2023-05-20 10:32:01 -0300

    feat: export get-user/get-user" get-user/protocols"

--- api_stream/src/controllers/user-controller/index.ts
+++ api_stream/src/controllers/user-controller/index.ts
@@ -7,5 +7,8 @@ export * from "./delete-user/protocols"
 export * from "./get-users/get-users"
 export * from "./get-users/protocols"
 
+export * from "./get-user/get-user"
+export * from "./get-user/protocols"
+
 export * from "./update-user/update-user"
-export * from "./update-user/protocols"
+export  * from "./update-user/protocols"
